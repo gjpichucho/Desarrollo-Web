b2194a4a49ceb0007c740d364ab075d591534168
Galo Pichucho <38517322+gjpichucho@users.noreply.github.com>
2020-07-01 16:07:01 -0500

    ejercicio 1

---
+++ ej1.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Ejercicio 1</title>
+</head>
+<body>
+	<h1>Universidad de las Fuerzas Armadas Espe</h1>
+	<h2>Grupo: Pichucho Galo - Rivera Santiago - Vivas Santiago</h2>
+	<h3>Ejercicio 1</h3>
+	<p>Crear un programa en PHP, que pida el numero de una tabla (numero del 1 al 10);
+y cuyo resultado sea la impresión de la tabla de multiplicar hasta el 20.</p>
+	<form name="forma" method="POST" action="res1.php">
+		<label for="num1">Ingrese un numero del 1 al 10</label>
+		<input type="number" id=num name="num" min=0 max=10>
+		<input type="submit" value="Enviar">
+
+	</form>
+ <a href="./index.html">Regresar</a>
+</body>
+</html>
---
+++ res1.php
@@ -0,0 +1,30 @@
+<?php
+    $num= $_POST["num"];
+   
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Ejercicio 1</title>
+</head>
+<body>
+	<h1>Universidad de las Fuerzas Armadas Espe</h1>
+	<h2>Grupo: Pichucho Galo - Rivera Santiago - Vivas Santiago</h2>
+	<h3>Resultado Ejercicio 1</h3>
+	<?php
+    for ($i=0; $i<=20;$i++){
+        $res= $num * $i;
+        ?>
+        <?php echo $num ?> * <?php echo $i ?> = <?php echo $res ?>
+        <br>
+    <?php
+    }
+    ?>
+
+    <a href="./ej1.php">Regresar</a> <br>
+    <a href="./index.html">Menu Principal</a>
+	
+</body>
+</html>
